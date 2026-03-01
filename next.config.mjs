56afc2f5ddddd31195f9d79ff20bc461b1d47dc2
JummyJoeJackson <diegogonaguilar@gmail.com>
2026-02-28 19:50:13 -0500

    Added next.js mdx support file

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Configure pageExtensions to support MDX
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+  // Optionally, add logic for images or experimental features here
+}
+
+export default nextConfig
